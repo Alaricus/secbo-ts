90b5eaf6e2c0816979ced77d289120bf0b52ffb6
Alaric von Teplitz <alaric2000@gmail.com>
2021-07-25 18:35:12 -0700

    Bring functionality on par with original

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,25 @@
-import { useRef } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import ImageUploader from './ImageUploader';
 
 const App = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [canvasReady, setCanvasReady] = useState(false);
 
   // TODO: Why do I need to define the above as null and why does the receiving component have to worry about it
   // potentially being null? I have the ref in the JSX below, it could never be null because it's always present.
 
+  useEffect(() => {
+    if (canvasRef.current && !canvasReady) {
+      setCanvasReady(true);
+    }
+  }, [canvasRef]);
+
   return (
     <div className="App">
       <h2>secbo</h2>
-      <ImageUploader canvas={canvasRef.current} />
+      {
+        canvasReady && <ImageUploader canvas={canvasRef.current} />
+      }
       <canvas ref={canvasRef} />
     </div>
   );
--- src/components/ImageUploader.tsx
+++ src/components/ImageUploader.tsx
@@ -1,10 +1,11 @@
-import { FC, useState, DragEvent } from 'react';
+import { FC, useState, DragEvent, ChangeEvent, useEffect } from 'react';
 
 interface ImageData {
   text: string,
   binary: string,
   image: HTMLImageElement | null,
   name: string,
+  dl: string,
 }
 
 interface ImageUploaderProps {
@@ -12,34 +13,111 @@ interface ImageUploaderProps {
 }
 
 const ImageUploader: FC<ImageUploaderProps> = ({ canvas }) => {
-  const [imageData, setImageData] = useState<ImageData>({ text: '', binary: '', image: null, name: '' });
+  const [imageData, setImageData] = useState<ImageData>({ text: '', binary: '', image: null, name: '', dl: '' });
+  const [pixels, setPixels] = useState(0);
+  const [freePixels, setFreePixels] = useState(0);
 
-  const refresh = (image: HTMLImageElement, name: string) => {
+  useEffect(() => {
+    if (imageData?.image) {
+      const tempPixels = imageData.image.width * imageData.image.height;
+      const tempFreePixels = tempPixels - imageData.binary.length;
+
+      setPixels(tempPixels);
+      setFreePixels(tempFreePixels);
+    }
+  }, [imageData.text, imageData.binary]);
+
+  let ctx: CanvasRenderingContext2D | null = null;
+  if (canvas) {
+    ctx = canvas.getContext('2d');
+  }
+
+  const textToBinary = (txt: string): string => `[secbo]${txt}[/secbo]`.split('').map(c => `${c.charCodeAt(0).toString(2)} `).join('');
+
+  const binaryToText = (bin: string): string => bin.split(' ').map(b => String.fromCharCode(parseInt(b, 2))).join('');
+
+  const reset = (image: HTMLImageElement | null, name: string): void => {
     setImageData({
       text: '',
       binary: '1011011 1110011 1100101 1100011 1100010 1101111 1011101  1011011 101111 1110011 1100101 1100011 1100010 1101111 1011101',
       image,
       name,
+      dl: '',
     });
   };
 
-  const updateCanvas = () => {
+  const updateCanvas = (image: HTMLImageElement): void => {
     // TODO: See if this "if" null check can be avoided.
-    if (imageData.image && canvas) {
-      const ctx = canvas.getContext('2d');
-      // TODO: See if this "if" null check can be avoided.
-      if (ctx) {
-        ctx.canvas.width = imageData.image.width;
-        ctx.canvas.height = imageData.image.height;
-        ctx.drawImage(imageData.image, 0, 0);
+    if (ctx) {
+      ctx.canvas.width = image.width;
+      ctx.canvas.height = image.height;
+      ctx.drawImage(image, 0, 0);
+    }
+  };
+
+  const readAlpha = (): string | null => {
+    // TODO: See if this "if" null check can be avoided.
+    if (ctx) {
+      const ctxImageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
+      const bin = ctxImageData.data.reduce((acc, value, index) => {
+        if (index > 0 && (index + 1) % 4 === 0) {
+          if (value === 253) {
+            const tempAcc = `${acc} `;
+            return tempAcc;
+          }
+          if (value === 254) {
+            const tempAcc = `${acc}1`;
+            return tempAcc;
+          }
+          if (value === 255) {
+            const tempAcc = `${acc}0`;
+            return tempAcc;
+          }
+        }
+        return acc;
+      }, '');
+
+      const text = binaryToText(bin);
+
+      if (text.startsWith('[secbo]') && text.substring(0, text.length - 1).endsWith('[/secbo]')) {
+        return text.substring(7, text.length - 9);
+      }
+    }
+
+    return null;
+  };
+
+  const writeAlpha = (binary: string): void => {
+    // TODO: See if this "if" null check can be avoided.
+    if (ctx) {
+      const ctxImageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
+      if (ctxImageData.data.length / 4 >= binary.length) {
+        // This sets everything to 255, so no need to handle zeroes
+        ctxImageData.data.forEach((datum, index) => {
+          if (index > 0 && (index + 1) % 4 === 0) {
+            ctxImageData.data[index] = 255;
+          }
+        });
+
+        binary.split('').forEach((digit, index) => {
+          if (digit === '1') {
+            ctxImageData.data[(index * 4) + 3] = 254;
+          }
+
+          if (digit === ' ') {
+            ctxImageData.data[(index * 4) + 3] = 253;
+          }
+        });
+
+        ctx.putImageData(ctxImageData, 0, 0);
+      } else {
+        // TODO: Set up a mechanism for reporting errors to the user
+        alert('The image was too small to contain all this data.');
       }
     }
   };
 
-  const readAlpha = () => { console.log('readAlpha()'); };
-  const getDownload = () => { console.log('getDownload()'); };
-
-  const uploadImage = (e: DragEvent<HTMLDivElement>) => {
+  const uploadImage = (e: DragEvent<HTMLDivElement>): void => {
     e.preventDefault();
     const fr = new FileReader();
     const img = new Image();
@@ -57,39 +135,96 @@ const ImageUploader: FC<ImageUploaderProps> = ({ canvas }) => {
     fr.onload = event => {
       try {
         img.onload = () => {
-          refresh(img, file.name);
-          updateCanvas();
-          readAlpha();
-          getDownload();
+          updateCanvas(img);
+          const text = readAlpha();
+          setImageData({
+            ...imageData,
+            image: img,
+            name: file.name,
+            text: text || '',
+            binary: textToBinary(text || ''),
+          });
         };
         // TODO: This seems like a bad way of doing things. Sesearch further.
         img.src = event?.target ? event.target.result as string : '';
       } catch (err) {
+        // TODO: Set up a mechanism for reporting errors to the user
         console.log('File failed to load.');
       }
     };
   };
 
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>): void => {
+    writeAlpha(textToBinary(e.target.value));
+    setImageData({
+      ...imageData,
+      text: e.target.value,
+      binary: textToBinary(e.target.value),
+      dl: canvas?.toDataURL('image/png') || '',
+    });
+  };
+
   return (
-    <div
-      className="ImageUploader"
-      onDrop={uploadImage}
-      onDragEnter={e => e.preventDefault()}
-      onDragOver={e => e.preventDefault()}
-    >
+    <>
+      <div
+        className="ImageUploader"
+        onDrop={uploadImage}
+        onDragEnter={e => e.preventDefault()}
+        onDragOver={e => e.preventDefault()}
+      >
+        {
+        imageData.image
+          ? (
+            <img
+              className="ImagePreview"
+              src={imageData.image.src}
+              alt="thumbnail"
+            />
+          ) : (
+            <span>drag an image file here</span>
+          )
+        }
+      </div>
       {
-      imageData.image
-        ? (
-          <img
-            className="ImagePreview"
-            src={imageData.image.src}
-            alt="thumbnail"
-          />
-        ) : (
-          <span>drag an image file here</span>
+        // These should probably be their own components and should be used in App.tsx, but what's
+        // a good way to pass all the data to them? A callback function on this component, or some
+        // kind of Context or state in App?
+      }
+      <div className="Info">
+        {imageData.image && `${pixels} total pixels, ` }
+        <span style={{ color: freePixels > 0 ? 'black' : 'red' }}>
+          { imageData.image && `${freePixels} still available` }
+        </span>
+      </div>
+      <textarea
+        rows={15}
+        cols={50}
+        value={imageData.text}
+        onChange={e => handleChange(e)}
+      />
+      <br />
+      { /* canvas was here in the JS version */ }
+      <br />
+      {
+        imageData.text.length > 0
+        && imageData?.image?.src
+        && freePixels >= 0
+        && (
+          <a
+            href={imageData.dl}
+            download={imageData.name}
+          >
+            download
+          </a>
         )
       }
-    </div>
+      {
+        imageData?.image?.src
+        && (
+          <button type="button" onClick={() => reset(null, '')}>clear</button>
+        )
+      }
+    </>
   );
 };
 
