88c1c45d0afb6b8f660707dd93fe10abc4891963
Alaric von Teplitz <alaric2000@gmail.com>
2021-08-12 15:54:18 -0700

    A few more tests for App

--- src/components/__test__/App.test.tsx
+++ src/components/__test__/App.test.tsx
@@ -2,9 +2,20 @@ import { render } from '@testing-library/react';
 import App from '../App';
 
 describe('<App />', () => {
+  const { getByTestId, getByText } = render(<App />);
+  const appElement = getByTestId('app');
+  const headerElement = getByText('secbo-ts');
+  const paragraphElement = getByText('v 1.2.1');
+
   it('should render without crashing', () => {
-    const { getByTestId } = render(<App />);
-    const appElement = getByTestId('app');
     expect(appElement).toBeVisible();
+  });
+
+  it('should contain the header', () => {
+    expect(appElement).toContainElement(headerElement);
+  });
+
+  it('should contain the paragraph with the version', () => {
+    expect(appElement).toContainElement(paragraphElement);
   });
 });
